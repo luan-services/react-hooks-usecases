cb3958a086af1cf336e17b8145bba1f768f6001e
luan-services <luandotservices@gmail.com>
2025-07-12 18:24:06 -0300

    largura da janela effect

--- components/use_effect_page_components/ContadorComTitulo.jsx
+++ components/use_effect_page_components/ContadorComTitulo.jsx
@@ -1,23 +1,27 @@
 import React, { useState, useEffect } from 'react';
 
 export const ContadorComTitulo = () => {
-  const [contagem, setContagem] = useState(0);
+	const [contagem, setContagem] = useState(0);
 
-  // este efeito atualiza o título da página
-  useEffect(() => {
-    // O título será atualizado após cada renderização em que 'contagem' mudar
-    document.title = `Você clicou ${contagem} vezes`;
-    console.log('Efeito do título executado!');
-    
-    // não há necessidade de cleanup aqui, por isso não tem return
-  }, [contagem]); // só disparada o useEffect quando contagem mudar.
+	const handleContagem = () => {
+		setContagem(contagem + 1)
+	}
 
-  return (
-    <div className="shadow-md border-gray-200 border-1 w-120 min-h-60 p-8 flex flex-col gap-4 items-center justify-center">
-      <p>você clicou {contagem} vezes</p>
-      <button className="min-w-16 min-h-8 px-2 py-1 rounded-lg bg-blue-500 text-white font-bold text-sm active:scale-95 transition" onClick={() => setContagem(contagem + 1)}>
-        Clique aqui
-      </button>
-    </div>
+	// este efeito atualiza o título da página
+	useEffect(() => {
+		// O título será atualizado após cada renderização em que 'contagem' mudar
+		document.title = `Você clicou ${contagem} vezes`;
+		console.log('Efeito do título executado!');
+		
+		// não há necessidade de cleanup aqui, por isso não tem return
+	}, [contagem]); // só disparada o useEffect quando contagem mudar.
+
+	return (
+		<div className="shadow-md border-gray-200 border-1 w-120 min-h-60 p-8 flex flex-col gap-4 items-center justify-center">
+		<p>você clicou {contagem} vezes</p>
+		<button className="min-w-16 min-h-8 px-2 py-1 rounded-lg bg-blue-500 text-white font-bold text-sm active:scale-95 transition" onClick={() => handleContagem()}>
+			Clique aqui
+		</button>
+		</div>
   );
 }
--- components/use_effect_page_components/LarguraDaJanela.jsx
+++ components/use_effect_page_components/LarguraDaJanela.jsx
@@ -4,25 +4,28 @@ export const LarguraDaJanela = () => {
   const [largura, setLargura] = useState(window.innerWidth);
 
   useEffect(() => {
-    // Função que atualiza o estado com a largura da janela
-    function handleResize() {
+    
+    // função que atualiza o estado com a largura da janela, precisa estar aqui dentro pois se estiver fora do useEffect, ela vai ser 
+    // recriada toda vez que o componente re-renderizar, isso vai causar o useEffect ser recriado junto toda vez, removendo o eventLisneter
+    // e criando de novo, o tempo todo sem necessidade.
+    const handleResize = () => {
       setLargura(window.innerWidth);
     }
     
-    // EFEITO: Adiciona o event listener quando o componente monta
+    // EFEITO: toda vez que a janela mudar de tamanho, a função handleResize vai disparar e atualizar o state 'largura.
     window.addEventListener('resize', handleResize);
-    console.log('Event listener ADICIONADO.');
+    console.log('event listener de resize da janela ADICIONADO.');
 
     // FUNÇÃO DE LIMPEZA: Essencial!
     // Esta função será executada quando o componente for desmontado.
     return () => {
       window.removeEventListener('resize', handleResize);
-      console.log('Event listener REMOVIDO.');
+      console.log('Event listener de resize da janela REMOVIDO.');
     };
   }, []); // <-- O array vazio garante que o efeito roda apenas uma vez (na montagem)
 
   return (
-    <div>
+    <div className="shadow-md border-gray-200 border-1 w-120 min-h-60 p-8 flex flex-col gap-4 items-center justify-center">
       <p>A largura atual da janela é: <strong>{largura}px</strong></p>
     </div>
   );
