76c718d4f4c459f4db993599d8aa02fc2cb354a6
luan-services <luandotservices@gmail.com>
2025-07-12 18:38:00 -0300

    set localstorage value

--- components/use_effect_page_components/SalvarFormLocalStorage.jsx
+++ components/use_effect_page_components/SalvarFormLocalStorage.jsx
@@ -1,28 +1,32 @@
 import React, { useState, useEffect } from 'react';
 
 export const SalvarFormLocalStorage = () => {
-  // Tenta carregar o nome do localStorage. Se não houver, usa uma string vazia.
-  const valorInicial = localStorage.getItem('nomeUsuario') || '';
-  const [nome, setNome] = useState(valorInicial);
+    // Tenta carregar o nome do localStorage. Se não houver, usa uma string vazia.
+    const valorInicial = localStorage.getItem('nomeUsuario') || '';
+    const [nome, setNome] = useState(valorInicial);
 
-  // Este efeito sincroniza o estado 'nome' com o localStorage
-  useEffect(() => {
-    localStorage.setItem('nomeUsuario', nome);
-    console.log(`Valor '${nome}' salvo no localStorage.`);
-    
-  }, [nome]); // O efeito roda toda vez que o 'nome' muda
+    const handleValueChange = (event) => {
+        setNome(event.target.value)
+    }
 
-  return (
-    <div>
-      <label>
-        Digite seu nome:
-        <input
-          type="text"
-          value={nome}
-          onChange={e => setNome(e.target.value)}
-        />
-      </label>
-      <p>Olá, {nome || 'estranho'}!</p>
-    </div>
-  );
+    // Este efeito sincroniza o estado 'nome' com o localStorage
+    useEffect(() => {
+        localStorage.setItem('nomeUsuario', nome);
+        console.log(`Valor '${nome}' salvo no localStorage.`);
+        
+    }, [nome]); // O efeito roda toda vez que o 'nome' muda
+
+    return (
+        <div className="shadow-md border-gray-200 border-1 w-120 min-h-60 p-8 flex flex-col gap-4 items-center justify-center">
+            <div className="flex flex-row gap-2 items-center">
+                <label className="flex">
+                    digite seu nome:
+                </label>
+                <input className="border-gray-500 border-1 rounded-sm p-1 outline-0 focus:border-blue-500" type="text" value={nome} onChange={(e) => handleValueChange(e)}/>
+            </div>
+            {nome &&
+                <p>valor {nome} salvo no localStorage</p>
+            }
+        </div>
+    );
 }
